a84421e8b364931295cbd6ff229bd61927367d3c
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-09-16 12:10:33 +0530

    updated to procure EC2 Instance with number of instance to be procured

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,13 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+resource "aws_instance" "centos_server"{
+    ami = "ami-030ff268bd7b4e8b5"
+    instance_type = "t2.micro"
+    count = 1
+
+    tags = {
+        Name = "IBM-DEVOPS-KUL"
+    }
+}
