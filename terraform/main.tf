e6e072c06983ecb693fa6aa1ed36713aadba7dc9
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-09-16 13:01:04 +0530

    updated to use output method to print Server details on screen

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "centos_server"{
     ami = "ami-030ff268bd7b4e8b5"
     instance_type = "t2.micro"
-    count = 1
+    #count = 1
 
     tags = {
         Name = "IBM-DEVOPS-KUL"
@@ -35,4 +35,16 @@ resource "aws_security_group" "centos_server_sg_group"{
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output "centos_server_public_ip" {
+    value = aws_instance.centos_server.public_ip
+}
+
+output "centos_server_private_ip" {
+    value = aws_instance.centos_server.private_ip
+}
+
+output "centos_server_id" {
+    value = aws_instance.centos_server.id
 }
