a4380a5148d822c721702bba5da7ac6d5e9faea2
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-09-16 16:57:34 +0530

    updated files with provisioners

--- terraform/attach_volume.tf
+++ terraform/attach_volume.tf
@@ -2,4 +2,21 @@ resource "aws_volume_attachment" "attach_volume" {
     device_name = "/dev/xvdb"
     volume_id = aws_ebs_volume.volume.id
     instance_id = aws_instance.centos_server.id
+
+    provisioner "remote-exec" {
+        connection{
+            type = "ssh"
+            user = "root"
+            password = "thinknyx@123"
+            host = aws_instance.centos_server.public_ip
+        }
+        inline = [
+            "df -h",
+            "mkfs -t ext4 /dev/xvdb",
+            "mkdir /opt/app",
+            "mount /dev/xvdb /opt/app",
+            "df -h"
+        ]
+    }
+
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,4 +11,21 @@ resource "aws_instance" "centos_server"{
     security_groups = [
         aws_security_group.centos_server_sg_group.name
     ]
+
+    provisioner "local-exec" {
+        command = "echo ${self.public_ip} > public_ip.txt"
+    }
+
+    provisioner "file" {
+
+        connection{
+            type = "ssh"
+            user = "root"
+            password = "thinknyx@123"
+            host = self.public_ip
+        }
+
+        source      = "public_ip.txt"
+        destination = "/tmp/public_ip.txt"
+    }
 }
